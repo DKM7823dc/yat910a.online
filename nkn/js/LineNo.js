a15d5df3ea0f32910a41cee23b4f0a805fdb9c7a
DKM7823dc <mkyusds82a@hotmail.com>
2025-09-17 22:11:00 +0800

    Add files via upload

---
+++ nkn/js/LineNo.js
@@ -0,0 +1,11 @@
+﻿var arr_wx =["@691ixupm","@595ujxkq","@433ykpmw"]; 
+            var kfhlja =["https://lin.ee/eyRSdOS","https://lin.ee/MkmFjjC","https://lin.ee/PXZcVJC"]; 
+        var wx_index = Math.floor((Math.random() * arr_wx.length));
+        var stxlwx = arr_wx[wx_index];
+            var kfhljas = kfhlja[wx_index]; 
+             
+     function showline() { 
+      
+  window.open(kfhljas);
+      
+      }
