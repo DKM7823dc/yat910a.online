831482a6d54afd2ec4e4fd92389471e724f6d157
DKM7823dc <mkyusds82a@hotmail.com>
2025-09-18 09:07:17 +0800

    Add files via upload

---
+++ meigu2/js/main22.js
@@ -0,0 +1,25 @@
+﻿function updateText(content) {
+    $('#zt1').text(content);
+    $('title').text(content);
+}
+
+updateText('Unlock real-time AI stock signal alerts');
+$('#zt2').text('Get customized advice in just two steps');
+
+$('#tx').attr('src', 'static/picture/AIcs.gif');
+
+$('#q1').text('Q1.Are you currently investing in stocks, options, or cryptocurrencies?');
+$('#q1_a').text('A. Yes');
+$('#q1_b').text('B. No');
+
+
+$('#q2').text('Q2.What type of investing do you prefer?');
+$('#q2_a').text('A. Short term');
+$('#q2_b').text('B. Long term');
+
+
+
+
+$('#answer').text('Thank you for your answer!');
+$('#answer_end').text('Live interactive learning and one-on-one learning guidance are now available. Add contact information to start your exclusive advice and guidance');
+$('#answer_button').text('Get my AI signal');
